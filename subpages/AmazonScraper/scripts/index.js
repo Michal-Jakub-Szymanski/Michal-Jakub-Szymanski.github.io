41f5074500e0810341835404a633a952a33d55a3
Michal-Jakub-Szymanski <micho.szymanski@gmail.com>
2022-03-02 18:25:15 +0100

    Update index.js

--- subpages/AmazonScraper/scripts/index.js
+++ subpages/AmazonScraper/scripts/index.js
@@ -1,6 +1,10 @@
 let [name, output] = [document.querySelector("#name"), document.querySelector("#price")]
 let url = "https://elektrykunlocked.xyz/AmazonScrape";
 
+fetch(url).then(data => data.json()).then(data =>{
+    name.textContent = data.name
+    output.textContent = data.price
+})
 let loop = setInterval(() => {
     fetch(url).then(data => data.json()).then(data =>{
         name.textContent = data.name
