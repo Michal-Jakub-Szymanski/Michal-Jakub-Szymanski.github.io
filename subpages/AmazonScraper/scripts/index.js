8db197c64fcb0b69f4400baac7ed58c0188c8e0d
Michal-Jakub-Szymanski <micho.szymanski@gmail.com>
2022-03-02 18:25:01 +0100

    Update index.js

--- subpages/AmazonScraper/scripts/index.js
+++ subpages/AmazonScraper/scripts/index.js
@@ -1,8 +1,9 @@
 let [name, output] = [document.querySelector("#name"), document.querySelector("#price")]
 let url = "https://elektrykunlocked.xyz/AmazonScrape";
 
-//fetch(`https://cors-anywhere.herokuapp.com/${url}`).then(resp => resp.json()).then(data => console.log(data))
-fetch(url).then(data => data.json()).then(data =>{
-    name.textContent = data.name
-    output.textContent = data.price
-})
+let loop = setInterval(() => {
+    fetch(url).then(data => data.json()).then(data =>{
+        name.textContent = data.name
+        output.textContent = data.price
+    })
+}, 1000*60*60);
